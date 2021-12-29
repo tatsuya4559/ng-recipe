920c54cdb0ccb9bb0ce8b00c8f170a64694f95c1
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-12-29 14:43:53 +0900

    routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Feature } from './header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +7,4 @@ import { Feature } from './header/header.component';
 })
 export class AppComponent {
   title = 'recepi-app';
-  activeFeature: Feature = 'recipes';
-
-  onNavigate(feature: Feature) {
-    this.activeFeature = feature;
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,15 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
-export type Feature = 'recipes' | 'shopping-list';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<Feature>();
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onSelect(feature: Feature) {
-    this.featureSelected.emit(feature);
   }
 
 }
