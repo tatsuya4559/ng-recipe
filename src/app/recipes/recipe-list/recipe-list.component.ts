1a5bfbec5f98926cfdd7c96338e830e6bad7a3a2
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-12-30 13:07:35 +0900

    remove unused import

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
